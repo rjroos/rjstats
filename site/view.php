8d66b1228c721684e543935fc0a5da275e99a8fc
rjroos <rjroos>
2005-04-10 12:23:24 +0000

    Maak dit robuuster.

--- site/view.php
+++ site/view.php
@@ -1,8 +1,11 @@
 <?
-include("rjstats.conf.inc");
+require("rjstats.conf.inc");
 $service  = @$_REQUEST['service'];
 $computer = @$_REQUEST['computer'];
 $start    = @$_REQUEST['start'];
 $f = RJSTATS_DATA."/".$computer."/php/"."/$service.php";
+if (!file_exists($f)) {
+	die("No such file or directory: $f\n");
+}
 include($f);
 ?>
